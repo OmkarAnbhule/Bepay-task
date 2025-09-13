525639ea0875275cc0db0152ca35564b52e97c3a
OmkarAnbhule <omkaranbhule75@gmail.com>
2025-09-13 21:04:51 +0530

    update minor changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { User, Order } = require('./models');
 
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
